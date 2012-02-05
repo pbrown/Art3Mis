bee204604bc4ba967581f27676bc10cfff87867d
Pooja Brown <pooja.brown@gmail.com>
2012-02-05 12:49:30 -0800

    Supporting Tail Recursion

--- src/recursive.erl
+++ src/recursive.erl
@@ -1,5 +1,5 @@
 -module(recursive).
--export([fac/1, len/1]).
+-export([fac/1, len/1, tail_fact/1, tail_fact/2, tail_len/1, tail_len/2]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 ->
@@ -9,3 +9,12 @@ fac(N) when N > 0 ->
 len([]) -> 0;
 len([_|X])->
     1 + len(X).
+
+
+tail_fact(N) -> tail_fact(N,1).
+tail_fact(0, Accumulator) -> Accumulator;
+tail_fact(N, Accumulator) when N > 0 -> tail_fact(N-1, N*Accumulator).
+
+tail_len(X) -> tail_len(X,0).
+tail_len([], Accumulator) -> Accumulator;
+tail_len([_|T], Accumulator) -> tail_len(T, Accumulator+1).
