e13c22e75744938c5f4c1f25ae19c6b4c89fafee
Pooja Brown <pooja.brown@gmail.com>
2012-02-05 12:13:16 -0800

    Compute length of a string recursively

--- src/recursive.erl
+++ src/recursive.erl
@@ -1,6 +1,11 @@
 -module(recursive).
--export([fac/1]).
+-export([fac/1, len/1]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 ->
     N * fac(N-1).
+
+
+len([]) -> 0;
+len([_|X])->
+    1 + len(X).
