db8069c79aa01967114cd5caeba7e2a70c486106
Pooja Brown <pooja.brown@gmail.com>
2012-02-11 15:48:09 -0800

    more recursion fun TODO: Need to write quicksort implementation

--- src/recursive.erl
+++ src/recursive.erl
@@ -1,5 +1,6 @@
 -module(recursive).
--export([fac/1, len/1, tail_fact/1, tail_fact/2, tail_len/1, tail_len/2]).
+-export([fac/1, len/1, tail_fact/1, tail_fact/2, tail_len/1, tail_len/2, duplicate/2, tail_duplicate/2, tail_duplicate/3,
+        reverse/1, tail_reverse/1, tail_reverse/2, sublist/2]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0 ->
@@ -18,3 +19,32 @@ tail_fact(N, Accumulator) when N > 0 -> tail_fact(N-1, N*Accumulator).
 tail_len(X) -> tail_len(X,0).
 tail_len([], Accumulator) -> Accumulator;
 tail_len([_|T], Accumulator) -> tail_len(T, Accumulator+1).
+
+
+duplicate(0,_)->[];
+duplicate(N,Term)->[Term|duplicate(N-1,Term)].
+
+tail_duplicate(N,Term)->
+    tail_duplicate(N,Term,[]).
+tail_duplicate(0,_,List)->
+    List;
+tail_duplicate(N,Term,List) when N > 0 ->
+   tail_duplicate(N-1, Term, [Term|List]).
+
+
+reverse([])->[];
+reverse([H|T])->
+    reverse(T)++[H].
+
+
+tail_reverse(L)->
+    tail_reverse(L,[]).
+tail_reverse([], Accumulator) -> Accumulator;
+tail_reverse([H|T],Accumulator)->
+     tail_reverse(T, [H|Accumulator]).
+
+sublist(_,0)->[];
+sublist([],_)->[];
+sublist([H|T], N) when N > 0 ->
+     [H|sublist(T, N - 1)].
+
