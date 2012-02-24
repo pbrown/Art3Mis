966d8209b8f498d05adddeca38889525b96bfb3c
Pooja Brown <pooja.brown@gmail.com>
2012-02-23 19:12:27 -0800

    starting to learn to spawn processes and message passing in erlang

---
+++ src/dolphins.erl
@@ -0,0 +1,25 @@
+-module(dolphins).
+-compile(export_all).
+
+dolphin1()->
+	receive
+		do_a_flip->
+			io:format("How about no?~n");
+		fish ->
+			io:format("So long and thanks for the fish ~n");
+		_ -> 
+			io:format("Heh, we are smarter than you hunmans. ~n")
+
+
+	end.
+
+
+dolphin2()->
+	receive
+		{From, do_a_flip}->
+			From ! "How about no?";
+		{From, fish}->
+			From ! "So long and thanks for the fish!";
+		_ ->
+			io:format("Heh, we are smarter than you humans. ~n")
+	end.
