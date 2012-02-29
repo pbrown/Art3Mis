3809f114cc9737937dcb44352d7ce6f515751cf4
Pooja Brown <pooja.brown@gmail.com>
2012-02-29 08:20:27 -0800

    doing stuff

---
+++ src/org/samples/java/Consumer.java
@@ -0,0 +1,25 @@
+package org.samples.java;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: pbrown
+ * Date: 2/28/12
+ * Time: 9:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Consumer extends Thread {
+
+   Fridge fridge;
+
+   Consumer(Fridge fridge){
+       this.fridge = fridge;
+   }
+
+    public void run(){
+        try{
+            while(true){
+
+            }
+        }
+    }
+}
