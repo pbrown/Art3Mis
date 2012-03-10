64687c362e6f6d39c18824e270a262d4807259bf
Pooja Brown <pooja.brown@gmail.com>
2012-03-10 14:18:52 -0800

    more learning of concurrent processes

---
+++ src/lib_misc.erl
@@ -0,0 +1,20 @@
+-module(lib_misc).
+-compile(export_all).
+
+on_exit(Pid, Fun)->
+    spawn(fun() ->
+        process_flag(trap_exit, true),
+        link(Pid),
+        receive
+           {'EXIT', Pid, Why} ->
+                Fun(Why)
+        end
+
+    end ).
+
+
+
+do_something()->
+
+
+
