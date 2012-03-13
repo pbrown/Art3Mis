999fda96444da3c358432568306b495ffb018104
Pooja Garg <pooja.garg@deem.com>
2012-03-12 20:08:48 -0700

    Added a timer process that expires when reminder message is sent. Utility functions for date_time interactions as well

--- src/event_server/src/event.erl
+++ src/event_server/src/event.erl
@@ -1,31 +1,54 @@
 -module(event).
 -compile(export_all).
--record(state, {server, name=" ", to_go=0}).
+-record(state, {server, name="", to_go=0}).
 
-start(EventName, Delay) ->
-    spawn(?MODULE, init, [self(), EventName, Delay]).
+start(EventName, DateTime) ->
+	spawn(?MODULE, init, [self(), EventName, DateTime]).
 
-start_link(EventName, Delay) ->
-    spawn_link(?MODULE, init, [self(), EventName, Delay]).
+start_link(EventName, DateTime) ->
+	spawn_link(?MODULE, init, [self(), EventName, DateTime]).
 
-init(Server, EventName, Delay) ->
-    loop(#state{server=Server, name=EventName, to_go=Delay}).
+init(Server, EventName, DateTime) ->
+	loop(#state{server=Server, name=EventName, to_go=time_to_go(DateTime)}).
 
-loop(S = #state{server=Server}) ->
-    receive
-        {Server, Ref, cancel} ->
-            Server ! {Ref, ok}
-    after S#state.to_go*1000 ->
-            Server ! {done, S#state.name}
-    end.
+cancel(Pid) -> 
+	Ref = erlang:monitor(process, Pid),
+	Pid ! {self(), Ref, cancel},
+	receive
+		{Ref, ok} ->
+			erlang:demonitor(Ref, [flush]),
+			ok;
+		{'DOWN', Ref, process, Pid, _Reason} ->
+			ok
+	end.
 
-cancel(Pid) ->
-    Ref = erlang:monitor(process, pid),
-    Pid ! {self(), Ref, cancel},
-    receive
-       {Ref, ok} ->
-            erlang:demonitor(Ref, [flush]),
-            ok;
-       {'DOWN', Ref, process, Pid, _Reason} ->
-            ok
-    end.
+% Run each event as a process that expires that dies when its time to send a reminder
+% If a cancel message is received, then .....
+loop(S = #state{server=Server, to_go=[T|Next]})->
+	receive
+		{Server, Ref, cancel} ->
+			Server ! {Ref, cancel} 
+	after T * 1000 ->
+		if Next =:= [] -> 
+			Server ! {done, S#state.name};
+		   Next =/= [] ->
+			loop(S#state{to_go=Next})
+		end
+	end.	
+
+%% Dealing with erlang limits of 50 days of milliseconds.
+normalize(N)->
+	Limit = 49 * 24 * 60 * 60,
+	[N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+time_to_go(Timeout={{_,_,_}, {_,_,_}}) ->
+	Now = calendar:local_time(),
+	io:format("Now: ~p", [Now]),
+	ToGo = calendar:datetime_to_gregorian_seconds(Timeout) - calendar:datetime_to_gregorian_seconds(Now),
+	io:format("Time to go: ~p", [ToGo]),
+	Secs = if ToGo > 0 -> ToGo;
+		  ToGo =<0 -> 0
+	       end,
+	normalize(Secs).
+
+
