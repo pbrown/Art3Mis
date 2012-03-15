dd67e579601fcd7762242562424f93f50bf8cec7
Pooja Brown <pooja.brown@gmail.com>
2012-03-15 06:59:35 -0700

    protocol for cancel, add and subscribing events for timer app

--- src/event_server/src/evserv.erl
+++ src/event_server/src/evserv.erl
@@ -15,19 +15,92 @@ start_link()->
     register(?MODULE, Pid=spawn_link(?MODULE, init, [])),
     Pid.
 
+terminate() ->
+    ?MODULE ! shutdown.
+
 init() ->
     loop(#state{events=orddict:new(), clients=orddict:new()}).
 
 loop(S=#state{}) ->
+    io:format("Registered Clients ~p~n", [S#state.clients]),
+    io:format("Registered Events ~p~n", [S#state.events]),
     receive
-       {Pid, MsgRef, {subscribe, Client}} ->
+        {Pid, MsgRef, {subscribe, Client}} ->
             Ref = erlang:monitor(process, Client),
-            io:format("Reference to Client is ~p~n", [Ref]),
             NewClients = orddict:store(Ref, Client, S#state.clients),
-            io:format("Subscribing clients are ~p~n", [NewClients]),
             Pid ! {MsgRef, ok},
             loop(S#state{clients=NewClients});
+        {Pid, MsgRef, {add, Name, Description, TimeOut}} ->
+            io:format("Adding Event ~p~n", [Name]),
+            case valid_datetime(TimeOut) of
+                true ->
+                    EventPid = event:start_link(Name, TimeOut),
+                    NewEvents = orddict:store(Name,
+                                              #event{name=Name,
+                                                     description=Description,
+                                                     pid=EventPid,
+                                                     timeout=TimeOut},
+                                              S#state.events),
+                    Pid ! {MsgRef, ok},
+                    loop(S#state{events=NewEvents});
+                false ->
+                    Pid ! {MsgRef, {error, bad_timeout}},
+                    loop(S)
+            end;
+        {Pid, MsgRef, {cancel, Name}} ->
+            io:format("Canceling Event ~p~n", [Name]),
+            Events = case orddict:find(Name, S#state.events) of
+                         {ok, E} ->
+                             event:cancel(E#event.pid),
+                             orddict:erase(Name, S#state.events);
+                         error ->
+                             S#state.events
+                     end,
+            Pid ! {MsgRef, ok},
+            loop(S#state{events=Events});
+        {done, Name} ->
+            io:format("Event fired ~p~n", [Name]),
+            case orddict:find(Name, S#state.events) of
+                {ok, E} ->
+                    send_to_clients({done, E#event.name, E#event.description},
+                                    S#state.clients),
+                    NewEvents = orddict:erase(Name, S#state.events),
+                    loop(S#state{events=NewEvents});
+                error ->
+                    %% This may happen if we cancel an event and
+                    %% it fires at the same time
+                    loop(S)
+            end;
+        shutdown ->
+            exit(shutdown);
+        {'DOWN', Ref, process, _Pid, _Reason} ->
+            loop(S#state{clients=orddict:erase(Ref, S#state.clients)});
+        code_change ->
+            ?MODULE:loop(S);
         Unknown ->
-                io:format("Unknown message is used: ~p~n", [Unknown]),
-                loop(S)
+            io:format("Unknown message: ~p~n",[Unknown]),
+            loop(S)
     end.
+
+
+%%% Internal Functions
+send_to_clients(Msg, ClientDict) ->
+    orddict:map(fun(_Ref, Pid) -> Pid ! Msg end, ClientDict).
+
+% Valid date time format    {{YYYY, MM, DD,}, {H,M,S}}
+valid_datetime({Date,Time}) ->
+    try
+        io:format("Date received: ~p ~p ~n", [Date, Time]),
+        calendar:valid_date(Date) andalso valid_time(Time)
+    catch
+        error:function_clause ->
+            false
+    end;
+valid_datetime(_) ->
+    false.
+
+valid_time({H,M,S}) -> valid_time(H,M,S).
+valid_time(H,M,S) when H >= 0, H < 24,
+                       M >= 0, M < 60,
+                       S >= 0, S < 60 -> true;
+valid_time(_,_,_) -> false.
