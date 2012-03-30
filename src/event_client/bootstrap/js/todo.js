d366934d1957e176736cf05eed6522b598c145f9
Pooja Garg <pooja.garg@deem.com>
2012-03-29 20:38:18 -0700

    updates to use backbone.js and underscore.js

--- src/event_client/bootstrap/js/todo.js
+++ src/event_client/bootstrap/js/todo.js
@@ -1,11 +1,11 @@
-$(function(){
+    $(function(){
 
     var Todo = Backbone.Model.extend({
 
         defaults: function(){
             return {
                 title: "empty todo...",
-                order: this,
+                order: Todos.nextOrder(),
                 done:false
             };
         },
@@ -23,14 +23,78 @@ $(function(){
         clear: function(){
             this.destroy();
         }
-    })
+    });
 
     var TodoList = Backbone.Collection.extend({
          model: Todo,
-         localStorage: new Store("todos-backbone"),
 
         done: function(){
             return this.filter(function(todo){return todo.get('done'); });
+        },
+
+        remaining: function(){
+            return this.without.apply(this, this.done());
+        },
+
+        nextOrder: function(){
+            if(!this.length) return 1;
+            return this.last().get('order')+1;
+        },
+
+        comparator: function(todo){
+            return todo.get('order');
         }
-    })
+    });
+
+    var Todos = new TodoList;
+
+
+    var TodoView = Backbone.View.extend({
+
+        tagName: "li",
+
+        template: _.template($('#item-template')).html()),
+
+        events: {
+            "click .toggle" : "toggleDone",
+            "dblclick .view" : "edit",
+            "click a.destroy" : "clear",
+            "keypress .edit" : "updateOnEnter",
+            "blur .edit"      : "close"
+        },
+
+        initialize: function(){
+            this.model.bind('change', this.render, this);
+            this.model.bind('destroy', this.remove, this);
+        },
+
+        render: function(){
+            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.toggleClass('done', this.model.get('done'));
+            this.input = this.$('.edit');
+            return this;
+        },
+
+        edit: function() {
+            this.$el.addClass("editing");
+            this.input.focus();
+        },
+
+        close: function() {
+            var value = this.input.val();
+            if (!value) this.clear();
+            this.model.save({title.value});
+            this.$el.removeClass("editing");
+        },
+
+        updateOnEnter: function(e){
+            if (e.keyCode == 13) this.close();
+        },
+
+        clear: function(){
+            this.model.clear();
+        }
+
+
+    });
 });
